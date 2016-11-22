c14e292bfd8f628105d43484b3943bc9956245db
Gaurav Gada <gauravgada@gmail.com>
2016-11-22 02:29:10 -0800

    init

---
+++ mongo-load.py
@@ -0,0 +1,16 @@
+import facebook, os
+from pymongo import *
+
+api = os.environ.get('FB_API')
+uri = os.environ.get('FROM_EMAIL')
+client = MongoClient(uri)
+db = client.facebook
+coll = db.free_and_for_sale
+
+last_update = coll.find_one(sort=[('updated_time', -1)])['updated_time']
+graph = facebook.GraphAPI(access_token=api)
+# 385662371445806 - UW Free & For Sale Group ID
+fbdata = graph.get_object(id='385662371445806/feed', since=last_update, date_format='U')['data']
+if fbdata:
+    ins_id = coll.insert(fbdata)
+client.close()
