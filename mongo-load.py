f497187218567e4669f8eace80792a5108a1f5c9
Gaurav Gada <gauravgada@gmail.com>
2016-11-22 16:12:48 +0530

    Rpi changes

--- mongo-load.py
+++ mongo-load.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 import facebook, os
 from pymongo import *
 
 api = os.environ.get('FB_API')
-uri = os.environ.get('FROM_EMAIL')
+uri = os.environ.get('MONGO_URI')
 client = MongoClient(uri)
 db = client.facebook
 coll = db.free_and_for_sale
@@ -10,7 +11,7 @@ coll = db.free_and_for_sale
 # Get max updated_time to load incrementally
 last_update = coll.find_one(sort=[('updated_time', -1)])['updated_time']
 graph = facebook.GraphAPI(access_token=api)
-# 385662371445806 - UW Free & For Sale Group ID
+# 385662371445806 - UW Free & For Sale Facebook Group ID
 fbdata = graph.get_object(id='385662371445806/feed', since=last_update, date_format='U')['data']
 if fbdata:
     ins_id = coll.insert(fbdata)
